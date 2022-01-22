e59aceb5da30089594adb86844d88e2a99c5cb42
ThFigueiredo <thfigueiredo.developer@gmail.com>
2022-01-22 18:09:13 -0300

    Lista de Cursos: Pipe para mostrar ícone

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,17 +2,21 @@ import { AppMaterialModule } from './app-material/app-material.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ErrorDialoComponent } from './componentes/error-dialo/error-dialo.component';
+import { CategoryPipe } from './pipes/category.pipe';
 
 
 
 @NgModule({
   declarations: [ //declaration colocam-se os components
-    ErrorDialoComponent
+    ErrorDialoComponent, CategoryPipe
   ],
   imports: [ //imports colocam-se os módulos
     CommonModule,
     AppMaterialModule
   ],
-  exports: [ErrorDialoComponent] // fazendo este exports pois preciso importá-lo em courses e outros
+  exports: [ // fazendo este exports pois preciso importá-lo em courses e outros
+    ErrorDialoComponent,
+    CategoryPipe
+  ]
 })
 export class SharedModule { }
